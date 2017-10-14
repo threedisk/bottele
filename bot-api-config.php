7fa7ed657fe17b5672096dcf8589f1bfd0858626
threedisk <threedisk@gmail.com>
2017-10-14 20:03:13 +0700

    Update bot-api-config.php

--- bot-api-config.php
+++ bot-api-config.php
@@ -4,7 +4,7 @@ if (!defined('HS')) {
     die('Tidak boleh diakses langsung.');
 }
 
-$token = '';
+$token = '471654282:AAEAGcBfGZdxhxUbaGV-l00wRuZZcw2IAv4';
 $debug = true;
 
-$chatid = '213567634';
+$chatid = '312183366';
